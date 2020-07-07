241e85dc3fd1afc7642a8a18b8e34fbb79e1d7ab
Santiago <santux09@gmail.com>
2020-07-07 20:04:04 -0300

    fin api

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 import "./styles/BadgesList.css";
+import { Link } from "react-router-dom";
+
 class BadgesList extends React.Component {
   render() {
+    if (this.props.badges.length === 0) {
+      return(
+      <div>
+        <h3>No badges were found</h3>
+        <Link to="/badges/new" className="btn btn-primary">Create new badge</Link>
+      </div>
+      )
+    }
     return (
       <ul className="list-unstyled BadgesList">
         {this.props.badges.map((badge) => {
